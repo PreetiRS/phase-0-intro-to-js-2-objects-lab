fcc4e5ff6aabf976c20fe4d3ad06549cc798f426
+ <+>
2021-03-09 15:57:44 -0800

    Still failing 1 task

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+const employee = {
+name: "Preeti",
+streetAddress: "123 New York"
+}
+//employee.name = "Sam";
+//employee.streetAddress = "11 Broadway"
+/*function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}.employee, {[key]:value})
+}
+
+function destructivelyUpdateEmployeeWithKeyandValue(employee, key, value) {
+  return Object
+}*/
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+  return Object.assign({}, employee, {name: 'Sam'}, {streetAddress: '11 Broadway'});
+}
+function destructivelyUpdateEmployeeWithKeyandValue(employee, key, value) {
+  employee['name'] = 'Sam';
+  employee['streetAddress'] = '12 Broadway';
+  return employee
+}
+function deleteFromEmployeeByKey (employee, key) {
+  return Object.assign({}, employee, {name: undefined});
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+  delete employee.name;
+  return employee;
+}
